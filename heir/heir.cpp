f66ab44305e6969d4f75b22ae96977a9a11e3b1e
Justin Davis <Justin.Davis@slu.edu>
2019-10-07 09:31:20 -0500

    Still getting floating point error. Added check to make sure its nonzero

--- heir/heir.cpp
+++ heir/heir.cpp
@@ -4,32 +4,38 @@ using namespace std;
 
 int main() {
 
-    int count, second;
+    long count, second;
     cin >> count >> second;
-
-    int possible = 0;
+    long rem = 0;
+    long possible = 0;
+    long num = count;
     
     while (count <= second) {
 
         bool unique[10]={false};
         long digits[6]={0};
-        int rem = 0;
-        int num = count;
+        rem = 0;
+        num = count;
         for (int i=5;i>=0;i--){
 
             rem=num%10;
+            cout << "Got past the remainder" << endl;
             num=num/10;
             digits[i]=rem;
             if (unique[digits[i]]==false) {unique[digits[i]]=true;}
-            else {break;}
+            else {cout << "In the break" << endl;break;}
 
         }
-        if ((count%(digits[0])==0) && count%(digits[1])==0 && count%(digits[2])==0 && count%(digits[3])==0 && count%(digits[4])==0 && count%(digits[5]==0)) {
+        if (digits[0]==0 || digits[1]==0 || digits[2]==0 || digits[3]==0 || digits[4]==0 || digits[5]==0) {cout << "What the frick" << endl; cout << count << endl;}
+        else {
+            if ((count%(digits[0])==0) && count%(digits[1])==0 && count%(digits[2])==0 && count%(digits[3])==0 && count%(digits[4])==0 && count%(digits[5]==0)) {
 
-            possible++;
+                possible++;
+                cout << "Got past possible" << endl;
 
+            }
         }
-
+        cout << "Got to the count" << endl;
         count++;
 
     }
