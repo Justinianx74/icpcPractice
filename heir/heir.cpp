02ce254c2d7fba87efadb1c0e97f3262f86091c1
Justin Davis <43012900+Justinianx74@users.noreply.github.com>
2019-10-18 16:01:27 -0500

    Update heir.cpp

--- heir/heir.cpp
+++ heir/heir.cpp
@@ -19,11 +19,10 @@ int main() {
         for (int i=5;i>=0;i--){
 
             rem=num%10;
-            cout << "Got past the remainder" << endl;
             num=num/10;
             digits[i]=rem;
             if (unique[digits[i]]==false) {unique[digits[i]]=true;}
-            else {cout << "In the break" << endl;break;}
+            else {break;}
 
         }
         if (digits[0]==0 || digits[1]==0 || digits[2]==0 || digits[3]==0 || digits[4]==0 || digits[5]==0) {cout << "What the frick" << endl; cout << count << endl;}
@@ -31,11 +30,9 @@ int main() {
             if ((count%(digits[0])==0) && count%(digits[1])==0 && count%(digits[2])==0 && count%(digits[3])==0 && count%(digits[4])==0 && count%(digits[5]==0)) {
 
                 possible++;
-                cout << "Got past possible" << endl;
 
             }
         }
-        cout << "Got to the count" << endl;
         count++;
 
     }
